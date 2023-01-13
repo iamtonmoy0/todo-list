a5eb3393016d5329019641508b183a64dc2fd855
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-14 03:07:27 +0600

    todo model struct added

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"time"
+
+	"github.com/thedevsaddam/renderer"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	collectionName string = "todo"
+	port           string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Title     string        `bson:"title"`
+		Completed bool          `bson:"completed"`
+		CreateAt  time.Time     `bson:"createAt"`
+	}
+
+	todo struct {
+	}
+)
+
 func main() {
 
 }
