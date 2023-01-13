9d333376a9135d7ff1b90db4511803d1a8b35db5
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-14 03:34:35 +0600

    serve port func added

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -42,6 +46,39 @@ func init() {
 	sess.SetMode(mgo.Monotonic, true)
 	db = sess.DB(dbName)
 }
+
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/", todoHandlers())
+	srv := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+	go func() {
+		log.Println("listening on port ", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen:%s\n", err)
+		}
+	}()
+}
+
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/", updateTodo)
+		r.Delete("/", deleteTodo)
+	})
+}
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
