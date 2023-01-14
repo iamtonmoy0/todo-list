cced4a7743af1fdcc0800c4bdfddd6d5b881a786
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-15 05:13:58 +0600

    update todo func added and error message added

--- main.go
+++ main.go
@@ -137,6 +137,40 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 		"message": "Todo deleted successfully",
 	})
 }
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+	if !bson.IsObjectIdHex(id) {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "the id is invalid",
+		})
+		return
+	}
+
+	var t todo
+
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+	if t.Title == "" {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "the title field is required",
+			//just for validation
+		})
+		return
+	}
+
+	if err := db.C(collectionName).Update(
+		bson.M{"_id": bson.ObjectIdHex(id)},
+		bson.M{"title": t.Title, "completed": t.Completed},
+	); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "failed to update todo",
+			"error":   err,
+		})
+		return
+	}
+}
 
 func main() {
 	stopChan := make(chan os.Signal) //for stopping server
